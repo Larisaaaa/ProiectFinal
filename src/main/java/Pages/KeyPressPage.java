46af3293bf4b15bba328ac3b2584db39a6c21fe2
Larisaaaa <puscaslarisa2@gmail.com>
2023-06-11 20:42:32 +0300

    proiect

---
+++ src/main/java/Pages/KeyPressPage.java
@@ -0,0 +1,14 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+
+public class KeyPressPage extends BasePage {
+        public KeyPressPage(WebDriver driver) {
+            super(driver);
+        }
+
+        By fieldLocator = By.id("my_field");
+        public void clickOnField(){driver.findElement(fieldLocator).sendKeys(Keys.ENTER);}
+}
---
+++ src/test/java/TestCases/KeyPressTestCases.java
@@ -0,0 +1,30 @@
+package TestCases;
+import Pages.BasePage;
+import Pages.KeyPressPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class KeyPressTestCases extends BasePage {
+    private KeyPressPage keyPressPage;
+
+    @BeforeMethod
+    public void setUp() {
+        super.setUp();
+        keyPressPage = new KeyPressPage(driver);
+    }
+
+    @Test
+    public void PressEnterAndCheckThatTheMessageReturnedIsCorrect() throws InterruptedException {
+        driver.findElement(By.linkText("Key Press")).click();
+        keyPressPage.clickOnField();
+
+        WebElement resultElement = driver.findElement(By.id("result"));
+        String actualResultText = resultElement.getText();
+        String expectedResultText = "You entered: ENTER";
+        Assert.assertEquals(actualResultText, expectedResultText, "Entered text is incorrect");
+    }
+}
+
